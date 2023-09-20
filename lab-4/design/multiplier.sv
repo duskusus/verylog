df8873f8c654bb06a9ca4e2e09f52ce75d1a9bea
Theodore Josephson <tdj4@illinois.edu>
2023-09-19 23:54:09 -0500

    created multiplier

---
+++ lab-4/design/multiplier.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/19/2023 11:47:49 PM
+// Design Name: 
+// Module Name: multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiplier(
+        input logic Clk, Reset_Load_Clear, Run,
+        input logic[7:0] SW,
+        output logic[3:0] hex_grid, 
+        output logic[7:0] hex_seg, Aval, Bval
+        output logic Xval
+    );
+endmodule
