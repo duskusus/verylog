36215943e6cafa4b60d7b3ae9d1c5669d79baaec
Theodore Josephson <tdj4@illinois.edu>
2023-09-19 23:40:48 -0500

    created lab-4 files

---
+++ lab-4/design/ripple_adder.sv
@@ -0,0 +1,57 @@
+module ripple_adder
+(
+	input  [15:0] A, B,
+	input         cin,
+	output [15:0] S,
+	output        cout
+);
+    
+    
+    logic[16:0] c; //unsure if I should use logic or wire here
+    assign c[0] = cin;
+    generate
+            for (genvar i = 0; i < 16; i = i + 1) begin: rca_16
+            full_adder fa(.A(A[i]), .B(B[i]), .cin(c[i]), .S(S[i]), .cout(c[i+1]));
+            end
+    endgenerate
+    assign cout = c[16];
+    //assign S = A + B;
+    //test testing line
+endmodule
+
+module adder_4
+(
+input logic[3:0] A, B,
+input logic cin,
+output logic[3:0] S,
+output logic cout
+);
+    logic c0;
+    full_adder fa0(.A(A[0]), .B(B[0]), .S(S[0]), .cin(cin), .cout(c0));
+    logic c1;
+    full_adder fa1(.A(A[1]), .B(B[1]), .S(S[1]), .cin(c0), .cout(c1));
+    logic c2;
+    full_adder fa2(.A(A[2]), .B(B[2]), .S(S[2]), .cin(c1), .cout(c2));
+    full_adder fa3(.A(A[3]), .B(B[3]), .S(S[3]), .cin(c2), .cout(cout));
+endmodule
+
+module adder_2
+(
+input logic[1:0] A, B,
+input logic cin,
+output logic[1:0] S,
+output logic cout
+);
+    logic c0;
+    full_adder fa0(.A(A[0]), .B(B[0]), .S(S[0]), .cin(cin), .cout(c0));
+    full_adder fa1(.A(A[1]), .B(B[1]), .S(S[1]), .cin(c), .cout(c1));
+endmodule
+
+module full_adder
+(
+    input logic A, B, cin,
+    output logic S, cout
+);
+    assign S = A ^ B ^ cin;
+    assign cout = (A & B) | (B & cin) | (A & cin);
+endmodule
---
+++ lab-4/simulation/addertest.sv
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/12/2023 03:51:12 PM
+// Design Name: 
+// Module Name: addertest
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module addertest();
+
+timeunit 10ns;
+timeprecision 1ns;
+
+logic Clk = 0; //create clock variable
+
+
+always #1 Clk = ~Clk; // invert clock every 1 timeunit ("#1" means unit delay)
+
+initial begin: CLOCK_INITIALIZATION
+    Clk = 0; //force clock to 0 so its not undefined
+end
+
+logic[15:0] A, B, S;
+logic cin, cout;
+
+//ripple_adder ra(.*);
+//lookahead_adder la(.*);
+select_adder sa(.*);
+
+int testcount = 1000;
+int errors = 0;
+
+always begin: TEST_VECTORS // runs once at start of simulation, must be named
+$display("simulation started");
+cin = 0;
+//for (int a = 0; a < 255; a++)
+//    begin
+//        for (int b = 0; b < 255; b++)
+//        begin
+//            #1 A = a;
+//                B = b;
+//            if(S != A + B)
+//                $display(S, " != ", A, " +", B, " = ", A + B);
+//        end
+//    end
+
+for (int i = 0; i < testcount; i++)
+    begin
+        #1  A = $urandom_range(0, 16'hffff);
+            B = $urandom_range(0, 16'hffff);
+            
+        #20
+        if(A + B != S)
+        begin
+            $display(i, ": ", S, " != ", A, " + ", B, " = ", A + B);
+            errors ++;
+        end
+        
+    end
+    $display("end of simulation, ", errors, " errors in ", testcount, " tests");
+    end
+endmodule
