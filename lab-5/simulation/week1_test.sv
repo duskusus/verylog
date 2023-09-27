9d1ef67db20218d0a38cd1e5834e51cbef080d84
Theodore Josephson <tdj4@illinois.edu>
2023-09-26 23:33:41 -0500

    created week 1 tests

---
+++ lab-5/simulation/week1_test.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module toplevel_test();
+timeunit 10ns;
+timeprecision 1ns;
+
+logic Clk = 0; //create clock variable
+
+
+always #1 Clk = ~Clk; // invert clock every 1 timeunit ("#1" means unit delay)
+
+initial begin: CLOCK_INITIALIZATION
+    Clk = 0; //force clock to 0 so its not undefined
+end
+
+// variable instantiations copied directly from module definition -Clk
+logic [15:0] SW;
+logic Reset ,Run, Continue;
+logic [15:0] LED;
+logic [7:0] hex_seg;
+logic [3:0] hex_grid;
+logic [7:0] hex_segB;
+logic [3:0] hex_gridB;
+
+slc3_testtop lc3(.*);
+
+
+always begin: TEST_VECTORS // runs once at start of simulation, must be named
+$display("simulation started");
+
+
+end
+endmodule
