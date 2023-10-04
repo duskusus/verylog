f86ba864b6c1d1ed38a07a695c42ad57778e7a17
Theodore Josephson <tdj4@illinois.edu>
2023-10-03 22:09:47 -0500

    seperated memory interface

--- lab-5/srcs/memory_interface.sv
+++ lab-5/srcs/memory_interface.sv
@@ -21,7 +21,24 @@
 
 
 module memory_interface(
-    
+    input logic[15:0] BUS, Data_to_CPU,
+    input logic MIO_EN, LD_MAR, LD_MDR, Clk, Reset
+    output logic[15:0] MAR, MDR, Data_from_CPU
     );
+
+always_ff
+begin
+    if(LD_MAR)
+        MAR <= BUS;
+
+    if(LD_MDR)
+    begin
+        if(MIO_EN)
+            MDR <= Data_from_SRAM;
+        else
+            MDR <= BUS;
+    end
+
+end
 
 endmodule
