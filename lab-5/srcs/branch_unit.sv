0555720558f6dd962d8e7cd490c28498796d8419
Theodore Josephson <tdj4@illinois.edu>
2023-10-04 20:10:27 -0500

    fixed branch unit

--- lab-5/srcs/branch_unit.sv
+++ lab-5/srcs/branch_unit.sv
@@ -1,17 +1,32 @@
 `timescale 1ns / 1ps
 
 module branch_unit(
+    input logic Clk, LD_CC, LD_BEN;
     input logic [15:0] BUS,
     input logic [2:0] IR,
     output logic BEN
     );
-    logic N, Z, P;
+    logic N, Z, P, Nc, Zc, Pc, BENs;
+
+    always_ff @ (posedge Clk)
+    begin
+
+        if(LD_BEN)
+            BEN <= BENc
+        
+        if(LD_CC)
+        begin
+        N <= Nc;
+        Z <= Zc;
+        P <= Pc;
+        end
+    end
     
     always_comb begin
-        N = BUS[15];
-        Z = ~(|BUS);
-        P = ~(N | Z);
+        Nc = BUS[15];
+        Zc = ~(|BUS);
+        Pc = ~(N | Z);
 
-        BEN = | ({N, Z, P} & IR);
+        BENc = | ({Nc, Zc, Pc} & IR);
     end
 endmodule
