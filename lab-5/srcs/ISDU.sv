00c2ad5fe27e23b7d29fcd26b077d7f3d1fb2ef6
Theodore Josephson <tdj4@illinois.edu>
2023-10-06 02:04:18 -0500

    fixed state 6 mdr issue

--- lab-5/srcs/ISDU.sv
+++ lab-5/srcs/ISDU.sv
@@ -283,6 +283,7 @@ module ISDU (   input logic         Clk,
 					LD_MAR = 1;
 					ADDR2MUX = 2'b01;
 					ADDR1MUX = 1'b1;
+					SR1MUX = 1'b1;
 				end
             S_07: //MAR<-B+off6
 				begin
