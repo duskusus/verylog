22b6c74155e75f999460268b65edd2b1f4aa81c0
Theodore Josephson <tdj4@illinois.edu>
2023-11-01 00:17:15 -0500

    saving lab 7.1 working block diagram, starting 7.2, no files yet

---
+++ lab-7/lab7_1_provided_fa23_2/toplevel.sv
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Zuofu Cheng
+// 
+// Create Date: 12/11/2022 10:48:49 AM
+// Design Name: 
+// Module Name: mb_usb_hdmi_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// Top level for mb_lwusb test project, copy mb wrapper here from Verilog and modify
+// to SV
+// Dependencies: microblaze block design
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mb_usb_hdmi_top(
+    input logic Clk,
+    input logic reset_rtl_0,
+    
+    //USB signals
+    input logic [0:0] gpio_usb_int_tri_i,
+    output logic gpio_usb_rst_tri_o,
+    input logic usb_spi_miso,
+    output logic usb_spi_mosi,
+    output logic usb_spi_sclk,
+    output logic usb_spi_ss,
+    
+    //UART
+    input logic uart_rtl_0_rxd,
+    output logic uart_rtl_0_txd,
+    
+    //HDMI
+    output logic hdmi_tmds_clk_n,
+    output logic hdmi_tmds_clk_p,
+    output logic [2:0]hdmi_tmds_data_n,
+    output logic [2:0]hdmi_tmds_data_p,
+        
+    //HEX displays
+    output logic [7:0] hex_segA,
+    output logic [3:0] hex_gridA,
+    output logic [7:0] hex_segB,
+    output logic [3:0] hex_gridB
+    );
+    
+    logic [31:0] keycode0_gpio, keycode1_gpio;
+    logic clk_25MHz, clk_125MHz, clk, clk_100MHz;
+    logic locked;
+    logic hsync, vsync, vde;
+    logic [3:0] red, green, blue;
+    logic reset_ah;
+    
+    assign reset_ah = ~reset_rtl_0;
+    
+    
+/*    //Keycode HEX drivers
+    HexDriver HexA (
+        .clk(Clk),
+        .reset(reset_ah),
+        .in({keycode0_gpio[31:28], keycode0_gpio[27:24], keycode0_gpio[23:20], keycode0_gpio[19:16]}),
+        .hex_seg(hex_segA),
+        .hex_grid(hex_gridA)
+    );
+    
+    HexDriver HexB (
+        .clk(Clk),
+        .reset(reset_ah),
+        .in({keycode0_gpio[15:12], keycode0_gpio[11:8], keycode0_gpio[7:4], keycode0_gpio[3:0]}),
+        .hex_seg(hex_segB),
+        .hex_grid(hex_gridB)
+    );
+*/  
+  blazeit420_wrapper blazeit420_i
+       (.HDMI_0_tmds_clk_n(hdmi_tmds_clk_n),
+        .HDMI_0_tmds_clk_p(hdmi_tmds_clk_p),
+        .HDMI_0_tmds_data_n(hdmi_tmds_data_n),
+        .HDMI_0_tmds_data_p(hdmi_tmds_data_p),
+        .clk_100MHz(Clk),
+        .reset_rtl_0(reset_ah),
+        .uart_rtl_0_rxd(uart_rtl_0_rxd),
+        .uart_rtl_0_txd(uart_rtl_0_txd));
+    
+endmodule
